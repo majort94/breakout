f643cec49928e0b90db16014ca8cad504caa333f
Nicolas San Jose <nas2375@rit.edu>
2015-11-06 11:18:50 -0500

    changed angleRowAll array to shapeRowAll array, started functions to reference surrounding bricks, apply effects to bricks, check brick status, and delete bricks

--- brick breaker/js/mapMaker.js
+++ brick breaker/js/mapMaker.js
@@ -41,17 +41,17 @@
 	var typeRow8 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 	var typeRowAll = [typeRow0, typeRow1, typeRow2, typeRow3, typeRow4, typeRow5, typeRow6, typeRow7, typeRow8];
 
-	var anglesRow0 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var anglesRow1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var anglesRow2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var anglesRow3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var anglesRow4 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var anglesRow5 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var anglesRow6 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var anglesRow7 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var anglesRow8 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var anglesRowAll = [anglesRow0, anglesRow1, anglesRow2, anglesRow3, anglesRow4, anglesRow5, anglesRow6, anglesRow7, anglesRow8];
-
+	var shapeRow0 = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+	var shapeRow1 = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+	var shapeRow2 = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+	var shapeRow3 = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+	var shapeRow4 = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+	var shapeRow5 = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+	var shapeRow6 = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+	var shapeRow7 = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+	var shapeRow8 = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+	var shapeRowAll = [shapeRow0, shapeRow1, shapeRow2, shapeRow3, shapeRow4, shapeRow5, shapeRow6, shapeRow7, shapeRow8];
+	
 	var map = {
 		numCols : typeRow0.length,
 		numRows : typeRowAll.length
@@ -59,17 +59,12 @@
 
 	};
 
-
-
-
 	var BLOCK = {
 		piece : canvas.width / map.numCols,
 		height0 : (canvas.height - 175) / map.numRows
 
 	}
 	var blocks = [];
-
-
 
 /*
 function makeMap(){
@@ -85,6 +80,7 @@ function makeMap(){
 
 }
 */
+
 function makeMap1(){
 	for(var i = 0; i < typeRowAll.length; i++){
 		for(var j = 0; j < typeRowAll[i].length; j++){
@@ -107,11 +103,10 @@ function makeMap1(){
 						tempFill = "brown";
 						break;
 				}
-				blocks.push(new Shape(((BLOCK.piece).toFixed()) * (j), (BLOCK.height0).toFixed() * i, (BLOCK.piece).toFixed() * 2, (BLOCK.height0).toFixed(), tempFill, typeRowAll[i][j], anglesRowAll[i][j], i, j));
+				blocks.push(new Shape(((BLOCK.piece).toFixed()) * (j), (BLOCK.height0).toFixed() * i, (BLOCK.piece).toFixed() * 2, (BLOCK.height0).toFixed(), tempFill, typeRowAll[i][j], shapeRowAll[i][j], i, j));
 			}
 		}
 	}
-
 }
 
 function Shape(x, y, w, h, fill, type, angle, row, rowIndex) {
@@ -122,6 +117,10 @@ function Shape(x, y, w, h, fill, type, angle, row, rowIndex) {
     this.fill = fill;
     this.type = type;
     this.angle = angle;
+    this.row = row;
+    this.rowIndex = rowIndex;
+    this.health = 1;
+    this.lifespan = null;	//in seconds
 }
 
 function drawMap(){
@@ -143,34 +142,93 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function applyEffectToSurroundingBricks(trigger, effect) {
-	//effect is just replacing the type with the types representing status effects
+// TO DO --------------- CHANGE TO RETURN REFERENCE ARRAY
+// remove code changing typeRowAll. create and fill array of surrounding brick indices
+function referenceSurroundingBricks(trigger, effect) {
+	
+	var maxRowIndex = typeRowAll.length;
+	//right now this works since all rows are consistent length
+	var maxWithinRowIndex = typeRowAll[0].length;
 
-	var maxXIndex = typeRowAll[0].length;
-	var maxYIndex = typeRowAll.length;
-
+	//statements check if surrounding bricks would be within the max width/height and if they are normal bricks (allowing them to be transformed to effect type)
+	// COMPASS KEY
 	// N
-	if ((trigger.row > 0) && (typeRowAll[trigger.row -1][trigger.index] == 0))
-		{ typeRowAll[trigger.row -1][trigger.index] = effect; }
-	//NE
-	if (((trigger.row > 0) && (trigger.index <= maxXIndex -2) && (typeRowAll[trigger.row][trigger.index -2] == 0)))
-		{ typeRowAll[trigger.row][trigger.index -2] = effect; }
-	//E
-	if ((trigger.index <= maxXIndex -2) && (typeRowAll[trigger.row][trigger.index +2] == 0))
-		{ typeRowAll[trigger.row][trigger.index +2] = effect; }
-	//SE
-	if (((trigger.row <= maxYIndex -2) && (trigger.index <= maxXIndex -2) && (typeRowAll[trigger.row +1][trigger.index +2] == 0)))
-		{ typeRowAll[trigger.row +1][trigger.index +2] = effect; }
-	//S
-	if ((trigger.row <= maxYIndex -2) && (typeRowAll[trigger.row +1][trigger.index] == 0))
-		{ typeRowAll[trigger.row +1][trigger.index] = effect; }
-	//SW
-	if (((trigger.row <= maxYIndex -2) && (trigger.index >= 2) && (typeRowAll[trigger.row +1][trigger.index -2] == 0)))
-		{ typeRowAll[trigger.row +1][trigger.index -2] = effect; }
-	//W
-	if ((trigger.index >= 2) && (typeRowAll[trigger.row][trigger.index -2] == 0))
-		{ typeRowAll[trigger.row][trigger.index -2] = effect; }
+	if ((trigger.row > 0) && (typeRowAll[trigger.row -1][trigger.rowIndex] == 1))
+		{ typeRowAll[trigger.row -1][trigger.rowIndex] = effect; }
+	// NE
+	if ((trigger.row > 0) && (trigger.rowIndex <= maxXIndex -2) && (typeRowAll[trigger.row][trigger.rowIndex -2] == 1))
+		{ typeRowAll[trigger.row][trigger.rowIndex -2] = effect; }
+	// E
+	if ((trigger.rowIndex <= maxXIndex -2) && (typeRowAll[trigger.row][trigger.rowIndex +2] == 1))
+		{ typeRowAll[trigger.row][trigger.trigger.rowIndex +2] = effect; }
+	// SE
+	if ((trigger.row <= maxYIndex -2) && (trigger.rowIndex <= maxXIndex -2) && (typeRowAll[trigger.row +1][trigger.rowIndex +2] == 1))
+		{ typeRowAll[trigger.row +1][trigger.rowIndex +2] = effect; }
+	// S
+	if ((trigger.row <= maxYIndex -2) && (typeRowAll[trigger.row +1][trigger.rowIndex] == 1))
+		{ typeRowAll[trigger.row +1][trigger.rowIndex] = effect; }
+	// SW
+	if ((trigger.row <= maxYIndex -2) && (trigger.rowIndex >= 2) && (typeRowAll[trigger.row +1][trigger.rowIndex -2] == 1))
+		{ typeRowAll[trigger.row +1][trigger.rowIndex -2] = effect; }
+	// W
+	if ((trigger.rowIndex >= 2) && (typeRowAll[trigger.row][trigger.rowIndex -2] == 1))
+		{ typeRowAll[trigger.row][trigger.rowIndex -2] = effect; }
 	// NW
-	if ((trigger.row > 0) && (trigger.index >= 2) && (typeRowAll[trigger.row -1][trigger.index -2] == 0))
-		{ typeRowAll[trigger.row -1][trigger.index -2] = effect; }
+	if ((trigger.row > 0) && (trigger.rowIndex >= 2) && (typeRowAll[trigger.row -1][trigger.rowIndex -2] == 1))
+		{ typeRowAll[trigger.row -1][trigger.rowIndex -2] = effect; }
+
+	//return referenceArray;
+}
+
+function applyEffectsToBricks(bricksArray, effect)
+{
+	for (var i=0; i < bricksArray.length; i++)
+	{
+		typeRowAll[bricksArray[i].row][bricksArray[i].rowIndex] = effect;
+		shapeRowAll[bricksArray[i].row][bricksArray[i].rowIndex].type = effect;
+		switch (effect)
+		{
+			//Freeze
+			case 5:
+				shapeRowAll[bricksArray[i].row][bricksArray[i].rowIndex].health = 2;
+				break;
+			//Burn
+			case 6:
+				shapeRowAll[bricksArray[i].row][bricksArray[i].rowIndex].lifespan = 3;
+				break;
+		}
+	}
+}
+
+// TO DO ------ ICE / BURN MECHANICS INTERACTION
+// remove lifespan, and change DOT to work with health, so that ice burns twice as fast as normal bricks
+function lethalStatusCheck(){
+	 for(var i = 0; i < typeRowAll.length; i++){
+		for(var j = 0; j < typeRowAll[i].length; j++)
+		{
+			//if there is a brick in element
+			if (shapeRowAll[i][j] != null)
+			{
+				// 0hp kill threshold
+				if (shapeRowAll[i][j].health == 0) 
+				{ 
+					deleteBrick(i, j); 
+				}
+				// find bricks affected by DOT
+		 		if (shapeRowAll[i][j].lifespan != null)
+		 		{
+		 			// 0lifespan kill threshold
+		 			if (shapeRowAll[i][j].lifespan == 0)
+		 			{
+		 				deleteBrick(i, j);
+		 			}
+		 		}
+			}
+		}
+	}
+}
+
+function deleteBrick(i, j){
+	typeRowAll[i][j] = 0;
+	shapeRowAll[i][j] = null;
 }
